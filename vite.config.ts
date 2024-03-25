c5ad01b8c5c07f4fdd57b2640d0ab6174a463f60
Adam Powers <apowers@ato.ms>
2024-03-24 21:08:47 -0700

    fix vite loading path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { readFileSync } from "fs";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -54,7 +55,7 @@ export default defineConfig(({ command, mode }) => {
     },
     resolve: {
       alias: {
-        "bjs-xr-html-mesh": "./index.ts",
+        "bjs-xr-html-mesh": resolve(__dirname, "./index.ts"),
       },
     },
   }
