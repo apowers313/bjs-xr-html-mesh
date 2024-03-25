8b0138e7e869b0deab8f5fc521bc355bee21227d
Adam Powers <apowers@ato.ms>
2024-03-24 17:19:12 -0700

    fix import paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
 import { readFileSync } from "fs";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
@@ -55,7 +54,7 @@ export default defineConfig(({ command, mode }) => {
     },
     resolve: {
       alias: {
-        "bjs-xr-html-mesh": resolve(__dirname, "./index.ts"),
+        "bjs-xr-html-mesh": "./index.ts",
       },
     },
   }
